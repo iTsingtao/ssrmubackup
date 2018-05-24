33c6e4dab9f21605590d33d9932cde80113b34e2
Ache1123 <zhangchaosq@gmail.com>
2018-05-24 18:24:49 +0800

    Add files via upload

--- ssrmu.sh
+++ ssrmu.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR mudbjson server
 #	Version: 1.0.25
-#	Author: Toyo. Ache1123 backup
+#	Author: Toyo
 #	Blog: https://doub.io/ss-jc60/
 #=================================================
 
